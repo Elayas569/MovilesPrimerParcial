ae635f799b7d53ff8f4aa3a1c98aef445ad51b71
Elayas569 <elias@cqo.mx>
2025-09-25 19:22:05 -0600

    elias' chamba

--- routes/products.py
+++ routes/products.py
@@ -9,5 +9,23 @@ products_bp = Blueprint('products', __name__)
 
 @products_bp.route("/register", methods=["POST"])
 def register():
-    # data
-    return
+    data = request.get_json()
+    if Products.query.filter_by(barcode=data['barcode']).first():
+        return jsonify({'message': 'Item already registered'}), 400
+
+    product_data = {
+        'barcode': data['barcode'],
+        'name': data['name'],
+        'buyPrice': data['buyPrice'],
+        'sellPrice': data['sellPrice'],
+        'stock': data['stock']
+    }
+    if 'marca' in data and data['marca'] is not None:
+        product_data['marca'] = data['marca']
+    if 'imageUrl' in data and data['imageUrl'] is not None:
+        product_data['imageUrl'] = data['imageUrl']
+
+    product = Products(**product_data)
+    db.session.add(product)
+    db.session.commit()
+    return jsonify({'message': 'Product registered'}), 201
