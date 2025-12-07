7a204df5fd13dff440219871445f511edd3f238f
Elayas569 <ortegaaguilarelias@gmail.com>
2025-12-06 18:40:49 -0600

    update githubactions

--- routes/health.py
+++ routes/health.py
@@ -3,12 +3,14 @@ from datetime import datetime
 
 health_bp = Blueprint('health', __name__)
 
+
 @health_bp.route('/', methods=['GET'])
 def health_check():
     try:
         return jsonify({
             "message": "Server is running my bro",
-            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+            "timestamp": datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+            "update": "updated con githubActions"
         }), 200
     except Exception as e:
         return jsonify({"error": str(e)}), 500
